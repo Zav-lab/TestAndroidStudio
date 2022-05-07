805223a7a1a4c9798f3f4039dac8b79dc2072daf
Zav-lab <72087524+Zav-lab@users.noreply.github.com>
2022-05-07 14:53:44 +0500

    New

--- app/src/main/java/com/example/myfirstprojectwithandroid/MainActivity.java
+++ app/src/main/java/com/example/myfirstprojectwithandroid/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.myfirstprojectwithandroid;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView resultTextView;
-    private EditText number_field_1, number_field_2;
-    private Button add_button;
 
 
     @Override
@@ -20,22 +22,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultTextView = findViewById(R.id.resultTextview);
-        number_field_1 = findViewById(R.id.number_filed_1);
-        number_field_2 = findViewById(R.id.number_field_2);
-        add_button = findViewById(R.id.add_button);
-
-
-        add_button.setOnClickListener(new View.OnClickListener() {
-
+        TextView main_text = findViewById(R.id.mainText);
+        Button btn_second = findViewById(R.id.btn_second);
+        btn_second.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                float num1 = Float.parseFloat(number_field_1.getText().toString());
-                float num2 = Float.parseFloat(number_field_2.getText().toString());
-                float res = num1 + num2;
-                resultTextView.setText(String.valueOf(res));
+//                showInfo(main_text.getText().toString(), btn_second);
+                showInfoAlert("Вы хотите закрыть приложение?");
+
+
 
             }
         });
+
+
     }
+
+    public void btnClick(View v) {
+        showInfo(((Button)v).getText().toString(), ((Button)v));
+
+    }
+
+    private void showInfoAlert(String text) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Большая подсказка")
+                .setMessage(text)
+                .setCancelable(false)
+                .setPositiveButton("Конечно", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    private void showInfo(String text, Button btn) {
+
+        btn.setText("Уже нажато");
+        btn.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+
+    }
+
 }
